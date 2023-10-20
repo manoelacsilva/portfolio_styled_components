eb8a280cfb4b3ceb8c6d0109c97848211c4b553a
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-19 21:37:16 -0300

    começando o projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/manoelacsilva.png" />
+    <Titulo>Manoela Coelho</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre Mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+      consequuntur aperiam non earum omnis suscipit quisquam amet beatae
+      provident nostrum, enim fuga cupiditate ea ullam possimus dolore debitis
+      error architecto?
+    </p>
+  </section>
+)
+
+export default Sobre

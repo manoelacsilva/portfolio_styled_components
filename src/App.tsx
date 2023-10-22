348196e7476546cd51420d667e6d1da03bd2aa9a
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-21 23:02:51 -0300

    criando arquivos para os temas light e dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+//import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaDark}>
       <EstiloGlobal />
       <Container>
         <Sidebar />
@@ -14,7 +18,7 @@ function App() {
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 

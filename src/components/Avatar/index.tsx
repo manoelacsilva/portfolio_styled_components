505e96119db7345c95e69d419e77581dcc772d77
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-21 22:04:28 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/manoelacsilva.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/manoelacsilva.png" />
-    <Titulo fontSize={20}>Manoela Coelho</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Manoela Coelho</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        manoelacsilva
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedora Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
